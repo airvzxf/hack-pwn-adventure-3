ba311edcaad0a7117eacb4d83eddbfcd4189225d
Israel Roldan <israel.alberto.rv@gmail.com>
2021-04-08 21:04:10 -0500

    Formatted the docker setup scripts.

--- src/server-vm/docker/setup/game_server.sh
+++ src/server-vm/docker/setup/game_server.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+
 su pwn3 -c "cd /opt/pwn3/client/PwnAdventure3_Data/PwnAdventure3/PwnAdventure3/Binaries/Linux/ && ./PwnAdventure3Server"
--- src/server-vm/docker/setup/master_server.sh
+++ src/server-vm/docker/setup/master_server.sh
@@ -1,13 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
 _term() {
-  if [ -z "$child" ]
-  then
-	return 0
+  if [ -z "$child" ]; then
+    return 0
   fi
-  echo "Caught SIGTERM signal!" 
-  trap - SIGTERM SIGKILL # clear the trap   
-  # kill sounds mean, but -s is graceful  
+  echo "Caught SIGTERM signal!"
+  trap - SIGTERM SIGKILL # clear the trap
+  # kill sounds mean, but -s is graceful
   kill -s -TERM "$child" 2>/dev/null
   su postgres -c "pg_dump --schema=public --format=plain --file=/opt/pwn3/postgres-data/data.sql master"
   exit 0
@@ -25,21 +24,21 @@ su postgres -c "psql -f $PWN3/setup/postgres_init.sql -d template1"
 su pwn3 -c "rm /opt/pwn3/client/PwnAdventure3_Data/PwnAdventure3/PwnAdventure3/Saved/Logs/*"
 
 if [ -f /opt/pwn3/postgres-data/data.sql ]; then
-    echo "Found data, making a backup now!"
-	cp -p /opt/pwn3/postgres-data/data.sql /opt/pwn3/postgres-data/data.sql."$(date +%Y%m%d_%H%M%S)"
-	echo "Restoring backup data!"
-	su postgres -c "psql -U postgres --dbname master -1 --file /opt/pwn3/postgres-data/data.sql"
+  echo "Found data, making a backup now!"
+  cp -p /opt/pwn3/postgres-data/data.sql /opt/pwn3/postgres-data/data.sql."$(date +%Y%m%d_%H%M%S)"
+  echo "Restoring backup data!"
+  su postgres -c "psql -U postgres --dbname master -1 --file /opt/pwn3/postgres-data/data.sql"
 else
-    echo "No backup data found!"
-	su pwn3 -c "psql master -f $PWN3/initdb.sql"
-	su pwn3 -c "psql master -f $PWN3/setup/postgres_master.sql"
-	su pwn3 -c "cd /opt/pwn3/server/MasterServer/ && ./MasterServer --create-admin-team Admin"
+  echo "No backup data found!"
+  su pwn3 -c "psql master -f $PWN3/initdb.sql"
+  su pwn3 -c "psql master -f $PWN3/setup/postgres_master.sql"
+  su pwn3 -c "cd /opt/pwn3/server/MasterServer/ && ./MasterServer --create-admin-team Admin"
 fi
 
-# cleanup all previous/old server master creds
+echo "Cleanup all previous/old server master creds"
 su pwn3 -c "psql master -f $PWN3/setup/postgres_cleanup.sql"
 
-# get new master server creds
+echo "Get new master server creds"
 su pwn3 -c "cd /opt/pwn3/server/MasterServer/ && ./MasterServer --create-server-account > /opt/pwn3/server/creds"
 
 # write the new creds to the server.ini
@@ -58,10 +57,8 @@ Password=$PW
 Instances=5
 EOL
 
-
 # run the server
-su pwn3 -c "cd /opt/pwn3/server/MasterServer/ && ./MasterServer"  &
+su pwn3 -c "cd /opt/pwn3/server/MasterServer/ && ./MasterServer" &
 
-
-child=$! 
+child=$!
 wait "$child"
